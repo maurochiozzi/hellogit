11266e569b7c1eddd74ab5acfb9a85a39d58dc20
maurochiozzi <maurochiozzi@hotmail.com>
2017-02-07 09:58:35 -0300

    Cadastro sem banco de dados. Com campos obrigatórios.

---
+++ helloword/src/dominio/Pessoa.java
@@ -0,0 +1,46 @@
+package dominio;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+	private String cargo;
+	private Double salario;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", sobrenome=" + sobrenome + ", cargo=" + cargo + ", salario=" + salario + "]";
+	}
+
+}
