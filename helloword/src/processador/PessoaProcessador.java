0f1938e3166f45631477b49caa10328d52755c69
maurochiozzi <maurochiozzi@hotmail.com>
2017-02-09 11:24:29 -0300

    bug fix - campo cpf só aceita numeros agora

--- helloword/src/processador/PessoaProcessador.java
+++ helloword/src/processador/PessoaProcessador.java
@@ -7,14 +7,14 @@ public class PessoaProcessador {
 	public void salvar(Pessoa pessoa){
 		PessoaDAO pessoaDao = new PessoaDAO();
 		
-		if(validate() == true){
+		if(validate(pessoa) == true){
 			pessoaDao.salvar(pessoa);
 		}
 		
 		
 	}
 	
-	public boolean validate(){
+	public boolean validate(Pessoa pessoa){
 		return true;
 	}
 }
